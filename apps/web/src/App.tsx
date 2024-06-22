45fcbfa9bd48eca5490751c932eb2ff94959bccf
Ourai Lin <ourairyu@gmail.com>
2024-06-22 10:42:19 +0800

    feat(web): add NFT list component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react'
+import { EthersWeb3ConfigProvider, MetaMask, OkxWallet } from '@ant-design/web3-ethers'
+import { Connector, ConnectButton } from '@ant-design/web3'
+import NftList from './components/nft-list'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <EthersWeb3ConfigProvider wallets={[MetaMask(), OkxWallet()]}>
+      <Connector>
+        <ConnectButton />
+      </Connector>
+      <NftList />
+    </EthersWeb3ConfigProvider>
   )
 }
 
